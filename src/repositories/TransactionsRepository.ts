a9046c90d7b1cfaf8a07c5ffdd5797595c835585
Vitor Bonet <vitorbonet@hotmail.com>
2020-04-18 14:59:21 -0300

    commit initial

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,10 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +19,28 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((tot, transaction) => {
+      return transaction.type === 'income' ? tot + transaction.value : tot;
+    }, 0);
+    const outcome = this.transactions.reduce((tot, transaction) => {
+      return transaction.type === 'outcome' ? tot + transaction.value : tot;
+    }, 0);
+    const total = income - outcome;
+
+    const balance = { income, outcome, total };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transation = new Transaction({ title, type, value });
+
+    this.transactions.push(transation);
+
+    return transation;
   }
 }
 
